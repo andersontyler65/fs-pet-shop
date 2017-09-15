8f3fde4715a10c53b298e1da34a935f1a8552933
Tyler Anderson <Tyler@Tylers-MacBook-Pro.local>
2017-09-15 12:35:03 -0600

    fifth-commit-pet-shop

--- expressServer.js
+++ expressServer.js
@@ -11,14 +11,30 @@ const port = process.env.PORT || 8000;
 app.disable('x-powered-by')
 
 app.get('/pets', function(req, res) {
-  fs.readFile(petsPath , 'utf8', function(err, data) {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) {
       console.error(err.stack)
       return res.sendStatus(500)
     }
     var pets = JSON.parse(data)
 
-    res.send(data)
+    res.send(pets)
+  })
+})
+
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      console.error(err.stack)
+      return res.sendStatus(500)
+    }
+    var id = Number.parseInt(req.params.id)
+    var pets = JSON.parse(data)
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404)
+    }
+    res.send(pets[id])
   })
 })
 
@@ -29,9 +45,5 @@ app.use(function(req, res) {
 app.listen(port, function() {
   console.log('Listening on port', port)
 })
-
-
-
-
 
 module.exports = app;
